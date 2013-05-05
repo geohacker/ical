932887b29b3f9c0a7ed4bf4c4cb5b6531b67cb12
Sajjad Anwar <sajjadkm@gmail.com>
2013-05-05 21:35:21 +0530

    Parser and usage guidelines

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ ical
 ====
 
 Script to parse iCal files and filter events based on a keyword.
+
+Usage
+------
+
+    python parser.py file.ics keyword
+
---
+++ parser.py
@@ -0,0 +1,18 @@
+import os, sys
+from icalendar import Calendar
+from dateutil import parser
+
+schedule = open(sys.argv[1])
+lookfor = sys.argv[2]
+format = '%A, %d. %B %Y %I:%M%p'
+
+calendar = Calendar.from_ical(schedule.read())
+for event in calendar.walk():
+	if event.has_key('SUMMARY'):
+		summary = event['SUMMARY']
+		if summary.find(lookfor) != -1 or summary.find(lookfor.swapcase()) != -1 or summary.find(lookfor.title()) != -1:
+			start = parser.parse(event['DTSTART'].to_ical()).strftime(format)
+			end = parser.parse(event['DTEND'].to_ical()).strftime(format)
+			print summary
+			print "From: ", start
+			print "To:", end
