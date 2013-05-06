f2fb6ed26393cbefd146f2c0fb7ee7f5e67cc9a1
Sajjad Anwar <sajjadkm@gmail.com>
2013-05-06 19:13:29 +0530

    Accept a series of keywords and export the filtered events to another ics file

--- parser.py
+++ parser.py
@@ -1,18 +1,34 @@
-import os, sys
+import sys
 from icalendar import Calendar
 from dateutil import parser
 
 schedule = open(sys.argv[1])
-lookfor = sys.argv[2]
+lookfor = sys.argv[2:]
+
 format = '%A, %d. %B %Y %I:%M%p'
+filtered = Calendar()
 
 calendar = Calendar.from_ical(schedule.read())
 for event in calendar.walk():
-	if event.has_key('SUMMARY'):
-		summary = event['SUMMARY']
-		if summary.lower().find(lookfor.lower()) != -1:
-			start = parser.parse(event['DTSTART'].to_ical()).strftime(format)
-			end = parser.parse(event['DTEND'].to_ical()).strftime(format)
-			print summary
-			print "From: ", start
-			print "To:", end
+    found = 0
+    if 'SUMMARY' in event.keys():
+        summary = event['SUMMARY']
+        for keyword in lookfor:
+            if summary.lower().find(keyword.lower()) != -1:
+                found = 1
+                continue
+            else:
+                found = 0
+                break
+        if found:
+            start = parser.parse(event['DTSTART'].to_ical()).strftime(format)
+            end = parser.parse(event['DTEND'].to_ical()).strftime(format)
+            print summary
+            print "From: ", start
+            print "To:", end
+            filtered.add_component(event)
+
+
+filtered_file = open('filtered.ics', 'wb')
+filtered_file.write(filtered.to_ical())
+filtered_file.close()
