bc70f189ca75d11f9a2cd020fe21e8bceae14ef2
Sajjad Anwar <sajjadkm@gmail.com>
2013-05-06 17:36:07 +0530

    Simplified the filter condition

--- parser.py
+++ parser.py
@@ -10,7 +10,7 @@ calendar = Calendar.from_ical(schedule.read())
 for event in calendar.walk():
 	if event.has_key('SUMMARY'):
 		summary = event['SUMMARY']
-		if summary.find(lookfor) != -1 or summary.find(lookfor.swapcase()) != -1 or summary.find(lookfor.title()) != -1:
+		if summary.lower().find(lookfor.lower()) != -1:
 			start = parser.parse(event['DTSTART'].to_ical()).strftime(format)
 			end = parser.parse(event['DTEND'].to_ical()).strftime(format)
 			print summary
